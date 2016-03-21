6288c385f209988ea071841f431543a16f9677f6
olivmonnier <omonnier.dev@gmail.com>
2016-03-22 00:38:40 +0100

    improve build front

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 var source = require('vinyl-source-stream'),
-    browserify = require('browserify'),
     streamify = require('gulp-streamify'),
     rename = require('gulp-rename'),
     uglify = require('gulp-uglify'),
     eslint = require('gulp-eslint'),
     sass = require('gulp-sass'),
     server = require('gulp-express'),
+    babel = require('gulp-babel'),
     gulp = require('gulp');
 
 gulp.task('lint', function() {
@@ -21,21 +21,21 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('./public/dist/'));
 });
 
-gulp.task('browserify', function() {
-  var bundleStream = browserify('./public/src/app.js').bundle();
-
-  bundleStream
-    .pipe(source('./public/src/app.js'))
+gulp.task('babelify', function() {
+  return gulp.src('public/src/app.js')
+  	.pipe(babel({
+  		presets: ['es2015']
+  	}))
     .pipe(streamify(uglify()))
     .pipe(rename('bundle.js'))
-    .pipe(gulp.dest('./public/dist'));
+  	.pipe(gulp.dest('public/dist'));
 });
 
-gulp.task('default', ['browserify', 'sass'], function() {
+gulp.task('default', ['babelify', 'sass'], function() {
   server.run(['server.js']);
 
-  gulp.watch('./public/src/**/*.js', ['browserify']);
+  gulp.watch('./public/src/**/*.js', ['babelify']);
   gulp.watch(['./public/sass/**/*.scss'], ['sass']);
 });
 
-gulp.task('build', ['lint', 'browserify', 'sass']);
+gulp.task('build', ['lint', 'babelify', 'sass']);
